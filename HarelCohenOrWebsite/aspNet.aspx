dcf97cf7f47bc43cb35ad16de11d471ebb3878a7
User Omer <User@ROOM302-17>
2025-11-30 15:38:28 +0200

    harel

--- HarelCohenOrWebsite/aspNet.aspx
+++ HarelCohenOrWebsite/aspNet.aspx
@@ -1,14 +1,8 @@
-﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" %>
-
-<script runat="server">
-
-</script>
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="aspNet.aspx.cs" Inherits="aspNet" %>
 
 <asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
-    <h2>form</h2>
-
-
+ <%=str  %>
 </asp:Content>
 
---
+++ HarelCohenOrWebsite/aspNet.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class aspNet : System.Web.UI.Page
+{
+    public string str;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        str ="<table>";
+        for (int i=1; i < 10; i++)
+        {
+            str += "<tr>";
+            for (int j = 1; j <= 10; j++)
+            {
+                str += "<td>";
+                str += j * i;
+                str += "</td>";
+            }
+                str += "</tr>";
+        }
+        str += "</table>";
+    }
+}
